8cde66568eba4f9d33fb5049cf9ae2f9f2662107
bcc290786 <bcc290786@gmail.com>
2017-02-26 04:45:42 +0800

    Update format.js
    update chinese format

--- src/public/src/js/format.js
+++ src/public/src/js/format.js
@@ -11,7 +11,7 @@
    * @return {float} - Formated size.
   */
   _Format.prototype.size = function (bytes) {
-    var sizes = ['o', 'ko', 'mo', 'go', 'to']
+    var sizes = ['B', 'KB', 'MB', 'GB', 'TB']
     if (bytes === 0) { return '0 b' }
     var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
     return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`
@@ -24,20 +24,20 @@
   */
   _Format.prototype.speed = function (bytes) {
     var bits = bytes * 8
-    var sizes = ['b', 'kb', 'mb', 'gb', 'tb']
+    var sizes = ['B', 'KB', 'MB', 'GB', 'TB']
     if (bits === 0) { return '0 b' }
     var i = parseInt(Math.floor(Math.log(bits) / Math.log(1024)), 10)
-    return `${Math.round(bits / Math.pow(1024, i), 2)} ${sizes[i]}/s`
+    return `${Math.round(bits / Math.pow(1024, i), 2)} ${sizes[i]}/秒`
   }
 
   /**
-   * Format Date to dd/mm/yyyy hh:mm:ss.
+   * Format Date to yyyy/mm/dd hh:mm:ss.
    * @param {Date} date - Date to format.
    * @return {string} - Formated date.
   */
   _Format.prototype.date = function (date) {
     date = new Date(date)
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}` /* + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() */
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}` /* + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() */
   }
 
   /**
@@ -57,10 +57,10 @@
 
     var returnString = ''
 
-    if (days > 0) { returnString += days + 'j - ' }
-    if (hours > 0) { returnString += hours + 'h ' }
-    if (minutes > 0) { returnString += minutes + 'm ' }
-    if (seconds > 0) { returnString += seconds + 's ' }
+    if (days > 0) { returnString += days + '天 - ' }
+    if (hours > 0) { returnString += hours + '時 ' }
+    if (minutes > 0) { returnString += minutes + '分 ' }
+    if (seconds > 0) { returnString += seconds + '秒 ' }
 
     return returnString
   }
